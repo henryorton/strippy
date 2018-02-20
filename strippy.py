e3e3478c073d82717ce2809a6e39360a14381274
henryorton <henryorton95@gmail.com>
2018-02-20 16:41:05 +1100

    Supports hsqc projections and peak plotting

--- strippy.py
+++ strippy.py
@@ -1,11 +1,13 @@
 import nmrglue as ng
 import numpy as np
 
-import matplotlib as mpl
-mpl.use('Agg')
+# import matplotlib as mpl
+# mpl.use('Agg')
 from matplotlib import pyplot as plt
 import argparse
 import warnings
+
+from pprint import pprint
 
 
 long_description = """
@@ -25,7 +27,7 @@ Plotting in the f1 dimension in from 0.5 to 6.0 ppm.
 if __name__=='__main__':
 	parser = argparse.ArgumentParser(description=long_description)
 	parser.add_argument('-d','--dataset',
-		help="directory of bruker processed data",type=str)
+		help="directory of nmrPipe 3D data",type=str)
 	parser.add_argument('-p','--peaks',
 		help="2 dimensional peak list with 'f3 f2' in ppm separated by spaces",
 		type=str)
@@ -38,6 +40,9 @@ if __name__=='__main__':
 	parser.add_argument('-r','--range',
 		help="Range to be plotted in f1 dimension e.g. '0.0 6.0'",
 		type=float, nargs=2)
+	parser.add_argument('-s','--hsqc',
+		help="directory of nmrPipe 2D HSQC data",
+		type=str)
 	args = parser.parse_args()
 else:
 	args = None
@@ -56,7 +61,7 @@ def load_peaks(fileName):
 	return peaks
 
 
-def load_spectrum(brukerProcDir):
+def load_spectrum_bruker(brukerProcDir):
 	dic, data = ng.bruker.read_pdata(brukerProcDir)
 	udic = ng.bruker.guess_udic(dic, data)
 	proc = dic['procs']
@@ -72,29 +77,36 @@ def load_spectrum(brukerProcDir):
 	return data/np.abs(data).mean(), scales
 
 
+def load_spectrum_pipe(pipeDir):
+	dic, data = ng.pipe.read(pipeDir)
+	ndim = len(data.shape)
+	scales = {i:ng.fileio.pipe.make_uc(dic, data, dim=i) for i in range(ndim)}
+	return data/np.abs(data).mean(), scales
+
+
 
 def make_contours(lowest, highest, number):
-	return (lowest + (highest-lowest)*np.arange(0,number)/float(number-1))**(2.**0.5)
+	return lowest+(highest-lowest)*(np.arange(0,number)/float(number-1))**(2.**0.5)
 
 
 
 if args:
 	width = args.width
 	peaks = load_peaks(args.peaks)
+	cm = plt.get_cmap('brg', len(peaks))
 	print("Loading data ...")
-	data, scales = load_spectrum(args.dataset)
+	data, scales = load_spectrum_pipe(args.dataset)
 	s1, s2, s3 = scales[0], scales[1], scales[2]
 
 	if args.contours is None:
 		std = np.std(data)
-		cont = make_contours(std,10*std,10)
+		cont = make_contours(5*std,50*std,10)
 	else:
 		cont = make_contours(*args.contours)
 
 	if args.range is None:
 		h1p, l1p = s1.ppm_limits()
 		h1i, l1i = 0, len(s1.ppm_scale())
-		print(h1p,l1p)
 	else:
 		l1p, h1p = args.range
 		maxi, mini = s1.ppm_limits()
@@ -130,26 +142,56 @@ if args:
 
 		with warnings.catch_warnings():
 			warnings.simplefilter("ignore")
-			ax.contour( strip, cont, extent=lims, colors='k', linewidths=1)
-			ax.contour(-strip, cont, extent=lims, colors='r', linewidths=1)
+			ax.contour( strip, cont, extent=lims, colors='k', linewidths=0.2)
+			ax.contour(-strip, cont, extent=lims, colors='r', linewidths=0.2)
 		ax.invert_xaxis()
 		ax.invert_yaxis()
 		ax.text(.5,.97,"{:3.1f}".format(c2p),horizontalalignment='center',
-			transform=ax.transAxes, rotation=90)
+			transform=ax.transAxes, rotation=90, backgroundcolor='1')
 
 		ax.set_xticks([c3p])
-		ax.set_yticks(np.arange(int(l1p)+1,h1p,0.5))
+		ax.set_yticks(np.linspace(int(l1p),h1p,40))
 		ax.yaxis.grid(linestyle='dotted')
 		if hide_axis:
 			ax.yaxis.set_ticklabels([])
 			ax.yaxis.set_ticks_position('none')
 		hide_axis = True
+
+		ax.set_title(str(i), color=cm(i))
 		
 	
 	fig.autofmt_xdate(rotation=90, ha='center')
 	fileName = 'strips.pdf'
 	fig.savefig(fileName, bbox_inches='tight')
 	print("{} file written".format(fileName))
+
+
+	if args.hsqc:
+		data, scales = load_spectrum_pipe(args.hsqc)
+		s2, s3 = scales[0], scales[1]
+	else:
+		data = np.ptp(data, axis=0)
+
+	std = np.std(data)
+	cont = make_contours(5*std,30*std,10)
+	
+	fig = plt.figure(figsize=(11,8))
+	ax = fig.add_subplot(111)
+
+	ax.contour(data, cont, colors='k', extent=s3.ppm_limits()+s2.ppm_limits(),
+		linewidths=0.2)
+	for i, peak in enumerate(peaks):
+		ax.plot(*peak, color='r', marker='x')
+		ax.annotate(str(i), xy=peak, color=cm(i))
+
+	ax.invert_xaxis()
+	ax.invert_yaxis()
+	# ax.set_xlim(11, 5.5)
+	# ax.set_ylim(136,100)
+	fig.savefig('hsqc.pdf')
+
+
+
 
 
 
