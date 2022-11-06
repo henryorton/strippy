4696d27eb8135ab30e609c165ce6da53da3e958a
Henry Orton <henryorton95@gmail.com>
2022-11-06 13:01:59 +1100

    Bruker 3d file read with logging

--- axis.py
+++ axis.py
@@ -1,7 +1,11 @@
 """A class for managing a ppm scale"""
 from dataclasses import dataclass
+from typing import Union
 import numpy as np
 import numpy.typing as npt
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -9,36 +13,34 @@ class Axis:
     """
     Stores information about the axis ppm values.
     Can provide unit conversion between points, ppm and Hz values.
-    Note all scales are returned in descending order
+    The Axis must be ordered with values left to right.
 
     points : number of points along the axis
     carrier : centre frequency of the axis in Hz
     spectral_width : spectral width of the axis in Hz
-    observed_frequency : acquired NMR spectrometer frequency in MHz
-    dimension : dimension of the current axis
+    observed_frequency : NMR spectrometer frequency in MHz
     label : nucleus identifier e.g. 1H, 15N, 13C
+    interpolate : if False, returned indices are integers rounded to the nearest index
+        if True, returned indices are floats representing a continuous index along the axis
     """
 
     points: int
     carrier: float
     spectral_width: float
     observed_frequency: float
-    dimension: int
     label: str
+    interpolate: bool = False
 
     def __str__(self):
-        return f"<Axis {self.dimension}:{self.label:>3}>"
+        return f"<Axis: {self.label}, {self.points} points in {self.ppm_limits} ppm>"
 
-    def __repr__(self):
-        return self.__str__()
+    def __post_init__(self):
+        logging.info(f"Created new axis: {self}")
 
     @property
     def hz_scale(self) -> npt.NDArray[np.float_]:
         """
-        Returns
-        -------
-        array : numpy.ndarray of floats
-                the axis values in Hz in descending order
+        Get an array of Hz values for the axis
         """
         return np.linspace(
             self.carrier + 0.5 * self.spectral_width,
@@ -49,38 +51,25 @@ class Axis:
     @property
     def ppm_scale(self) -> npt.NDArray[np.float_]:
         """
-        Returns
-        -------
-        array : numpy.ndarray of floats
-                the axis values in ppm in descending order
+        Get an array of ppm values for the axis
         """
         return self.hz_scale / self.observed_frequency
 
     @property
     def ppm_limits(self) -> tuple[float, float]:
         """
-        Returns
-        -------
-        tuple : (float, float)
-                the high and low ppm limits respectively
+        the left and right ppm limits respectively
         """
-        lo = (self.carrier + 0.5 * self.spectral_width) / self.observed_frequency
-        hi = (self.carrier - 0.5 * self.spectral_width) / self.observed_frequency
-        return lo, hi
+        lhs = (self.carrier + 0.5 * self.spectral_width) / self.observed_frequency
+        rhs = (self.carrier - 0.5 * self.spectral_width) / self.observed_frequency
+        return lhs, rhs
 
     def f(self, ppm: float) -> float:
         """
-        Unit conversion from ppm -> points location on the axis.
-        This returns the decimal location along the scale
+        Unit conversion from ppm -> index location on the axis.
+        This returns the continuous decimal index along the scale
 
-        Parameters
-        ----------
-        ppm : float
-
-        Returns
-        -------
-        point : float
-                the decimal point location along the axis
+        ppm : the desired ppm value to be translated
         """
         hzpp = self.spectral_width / float(self.points - 1)
         loc = (
@@ -90,86 +79,118 @@ class Axis:
 
     def i(self, ppm: float) -> int:
         """
-        Unit conversion from ppm -> points location on the axis.
-        This returns the closest integer location along the scale
+        Unit conversion from ppm -> index on the axis.
+        This returns the closest integer index along the scale
 
-        Parameters
-        ----------
-        ppm : float
-
-        Returns
-        -------
-        point : int
-                the integer point location closest the the provided ppm value
+        ppm : the desired ppm value to be translated
         """
-        return int(np.argmin(np.abs(self.ppm_scale - ppm)))
+        return round(self.f(ppm))
 
-    def __getitem__(self, slic: slice | float) -> slice | int:
+    def ppm_within_limits(self, ppm: float) -> bool:
         """
-        This magic method offers immediate translation between ppm and point
-        ranges.
+        Determine if ppm value is within limits of the axis.
 
-        Parameters
-        ----------
-        slic : slice or float
-                The ppm range to be translated. If [8.5:4.7] was provided, a new
-                slice containing the point ranges would be returned e.g. [55:560].
-                If a float is provided, the integer point location closest to
-                that point is returned.
-
-        Returns
-        -------
-        point range : slice or int
-                the integer range or point closes to the given ppm range or point
-                respectively
+        ppm : the ppm value to be tested.
         """
-        if isinstance(slic, slice):
-            if slic.start:
-                start = slic.start
+        lhs, rhs = self.ppm_limits
+        if lhs > rhs:
+            if lhs >= ppm >= rhs:
+                return True
+        else:
+            if lhs <= ppm <= rhs:
+                return True
+        return False
+
+    def __getitem__(self, key: slice | float) -> slice | int | float:
+        """
+        Translate between ppm slice and integer slice
+        A slice represents are range of values between <start> and <stop>
+
+        key : The ppm range to be translated.
+            If [8.5:4.7] was provided, a new slice containing the
+            point ranges would be returned e.g. [55:560].
+            If a float is provided, the index along the axis is returned.
+            This index could be a rounded int or a float, depnding on
+            the interpolation bool
+        """
+
+        if isinstance(key, slice):
+            if key.start is not None:
+                if not self.ppm_within_limits(key.start):
+                    raise IndexError(
+                        f"The value {key.start} ppm is outside the {self} limits of {self.ppm_limits}"
+                    )
+                start = key.start
             else:
                 start = self.ppm_limits[0]
-            if slic.stop:
-                stop = slic.stop
+            if key.stop is not None:
+                if not self.ppm_within_limits(key.stop):
+                    raise IndexError(
+                        f"The value {key.stop} ppm is outside the {self} limits of {self.ppm_limits}"
+                    )
+                stop = key.stop
             else:
                 stop = self.ppm_limits[1]
-            return slice(self.i(start), self.i(stop) + 1)
-        elif isinstance(slic, float):
-            return self.i(slic)
+
+            lhs, rhs = self.ppm_limits
+            if (lhs > rhs) != (start > stop):
+                raise IndexError(
+                    f"Slice order [{start}:{stop}] ppm is incorrect for {self} which has left to right order {self.ppm_limits} ppm"
+                )
+
+            if self.interpolate:
+                return slice(self.f(start), self.f(stop) + 1)
+            else:
+                return slice(self.i(start), self.i(stop) + 1)
+        elif isinstance(key, float):
+            if not self.ppm_within_limits(key):
+                raise IndexError(
+                    f"The value {key} ppm is outside the {self} limits of {self.ppm_limits}"
+                )
+            if self.interpolate:
+                return self.f(key)
+            else:
+                return self.i(key)
         else:
             raise TypeError("Must use a slice or float type in Axis getitem")
 
-    def new(self, slic: slice | float) -> "Axis" | tuple[int, int, float]:
+    def new(self, key: slice | float) -> Union["Axis", int, float]:
         """
         Return a new instance of the current axis constructed from a subset
         of the axis values.
 
-        Parameters
-        ----------
-        slic : slice or float
-                The ppm range to be taken for the new axis. If a float is provided,
-                something else happens
-
-        Returns
-        -------
-        Axis : object
-                the integer range or point closes to the given ppm range or point
-                respectively
+        key: The ppm range to be taken for the new axis.
+            If a float is provided, only the index for that point is returned.
+            That index could be an int or float depnding on the rounding settings
         """
-        if isinstance(slic, slice):
-            scale = self.hz_scale[self[slic]]
+        if isinstance(key, slice):
+            new_slice = self[key]
+            assert isinstance(new_slice, slice)
+            scale = self.hz_scale[new_slice]
             return self.__class__(
                 points=len(scale),
-                carrier=float(np.ptp(scale)),
-                spectral_width=0.5 * (scale.min() + scale.max()),
+                carrier=0.5 * (scale.min() + scale.max()),
+                spectral_width=float(np.ptp(scale)),
                 observed_frequency=self.observed_frequency,
-                dimension=self.dimension,
                 label=self.label,
             )
-        elif isinstance(slic, float):
-            loc = self.f(slic)
-            low = int(loc)
-            high = low + 1
-            weight = loc - low
-            return low, high, weight
-        else:
-            raise TypeError("Must use a slice or float type")
+        elif isinstance(key, float):
+            value = self[key]
+            if self.interpolate:
+                assert isinstance(value, float)
+                return value
+            else:
+                assert isinstance(value, int)
+                return value
+        raise TypeError("Must use a slice or float type")
+
+
+# a = Axis(
+#     points=1024,
+#     carrier=4.7 * 800.0,
+#     spectral_width=10.0 * 800.0,
+#     observed_frequency=800.0,
+#     label="1H",
+# )
+
+# print(a[2.0:8.0])
---
+++ bruker.py
@@ -0,0 +1,187 @@
+import os
+from typing import Any
+from spectrum import Spectrum
+from axis import Axis
+import re
+import numpy as np
+import numpy.typing as npt
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging._nameToLevel["INFO"])
+
+
+def reorder_submatrix(
+    mangled_data: npt.NDArray,
+    final_shape: tuple[int, ...],
+    submatrix_shape: tuple[int, ...],
+) -> npt.NDArray[np.float_]:
+    """
+    Reorder processed binary Bruker data to remove submatrix order
+    Returns an ordered array with correct shape
+
+    mangled_data : the one dimensional data array with submatrix index ordering
+    final_shape : the final shape of the array, e.g. (128, 256, 1024)
+    submatrix_shape : the shape of the submatrix
+    """
+    logging.debug(
+        (
+            f"Started submatrix reordering from shape {mangled_data.shape} "
+            f"with submatrices {submatrix_shape} to give shape {final_shape}"
+        )
+    )
+
+    if len(submatrix_shape) == 1 or len(final_shape) == 1:
+        return mangled_data
+
+    data = np.empty(final_shape, dtype=mangled_data.dtype)
+    submatrices_per_dimension = [
+        int(i / j) for i, j in zip(final_shape, submatrix_shape)
+    ]
+    total_submatrices = np.product(submatrices_per_dimension)
+    data_submatrix_shaped = mangled_data.reshape(
+        [total_submatrices] + list(submatrix_shape)
+    )
+
+    for submatrix_number, submatrix_index in enumerate(
+        np.ndindex(tuple(submatrices_per_dimension))
+    ):
+        submatrix_slices = [
+            slice(i * j, (i + 1) * j) for i, j in zip(submatrix_index, submatrix_shape)
+        ]
+        data[tuple(submatrix_slices)] = data_submatrix_shaped[submatrix_number]
+
+    return data
+
+
+def read_procs(file_name: str) -> dict[str, Any]:
+    """
+    Read Bruker processed data status parameters from file
+    Returns dictionary of key value pairs
+    """
+    logging.info(f"Reading Bruker processed data {file_name}")
+
+    line_match = re.compile(r"\#\#\$(.*)=[\s+]?(.*)")
+    value_match = re.compile("<(.*)>")
+    d = {}
+    with open(file_name) as f:
+        for line in f:
+            m = line_match.match(line)
+            if m:
+                key, value = m.groups()
+                v = value_match.match(value)
+                if v:
+                    d[key] = v.group(1)
+                else:
+                    try:
+                        f = float(value)
+                        i = int(f)
+                        if f == i:
+                            d[key] = i
+                        else:
+                            d[key] = f
+                    except ValueError:
+                        d[key] = value
+    logging.debug(f"Parsed Bruker processed data: {d}")
+    return d
+
+
+def read_clevels(file_name: str) -> npt.NDArray[np.float_]:
+    """
+    Read Bruker contour levels from file
+    """
+    logging.info(f"Reading Bruker contour levels: {file_name}")
+
+    with open(file_name) as f:
+        s = f.read().replace("\n", " ")
+    match = re.search(r"\#\#\$LEVELS=\s?\(.*\)(.*?)\#\#\$", s)
+    if match:
+        lvls = match.group(1)
+        contours = np.trim_zeros(np.array(lvls.split(), dtype=float))
+        logging.debug(f"Parsed Bruker contour levels: {contours}")
+        return contours
+    else:
+        raise KeyError(f"Contour levels were not found in file: {file_name}")
+
+
+def read_spectrum(file_name: str) -> npt.NDArray[np.int_]:
+    """
+    Read Bruker spectrum intensities from file
+    Can read 1r, 2rr and 3rrr files
+    Data is integer type.
+    WARNING submatrix ordering may be present
+    """
+    logging.info(f"Reading Bruker spectrum data file: {file_name}")
+
+    with open(file_name, "rb") as o:
+        return np.frombuffer(o.read(), dtype="<i4")
+
+
+def load_bruker(spectrum_directory: str) -> Spectrum:
+    """
+    Read a Bruker spectrum from file into Spectrum object
+    """
+    spectrum_path = None
+    clevels_path = None
+    procs: dict[int, dict[str, Any]] = {}
+
+    for file_name in os.listdir(spectrum_directory):
+        path = os.path.join(spectrum_directory, file_name)
+
+        if file_name in ("1r", "2rr", "3rrr", "4rrrr"):
+            spectrum_path = path
+
+        elif re.search("proc[0-9]?s", file_name):
+            match = re.search("proc([0-9])?s", file_name)
+            if match:
+                num = match.group(1)
+            else:
+                continue
+            if not num:
+                dimension = 1
+            else:
+                dimension = int(num)
+            procs[int(dimension)] = read_procs(path)
+
+        elif re.search("clevels", file_name):
+            clevels_path = path
+
+    if spectrum_path is None:
+        raise FileNotFoundError("Cannot locate spectrum data file 1r, 2rr or 3rrr")
+
+    axes = []
+    final_shape = []
+    submatrix_shape = []
+    for dimension in sorted(procs):
+        proc = procs[dimension]
+        axis = Axis(
+            points=proc["SI"],
+            carrier=proc["OFFSET"] * proc["SF"] - 0.5 * proc["SW_p"],
+            spectral_width=proc["SW_p"],
+            observed_frequency=proc["SF"],
+            label=proc["AXNUC"],
+        )
+        axes.append(axis)
+        final_shape.append(axis.points)
+        submatrix_shape.append(proc.get("XDIM", axis.points))
+
+    scale = 2.0 ** (-procs[1].get("NC_proc", 0.0))
+
+    # Read spectrum data and scale
+    data = read_spectrum(spectrum_path)
+    data = (
+        reorder_submatrix(
+            mangled_data=data,
+            final_shape=tuple(final_shape[::-1]),
+            submatrix_shape=tuple(submatrix_shape[::-1]),
+        )
+        / scale
+    )
+
+    # Read contour levels
+    if clevels_path:
+        contours = read_clevels(clevels_path)
+    else:
+        contours = np.array([])
+
+    return Spectrum(data=data, axes=tuple(axes[::-1]), contours=contours)
